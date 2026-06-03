500d60dd5b3a1a975bcf7c8843f8626f992e6b65
VishakhaPande <143498961+VishakhaPande@users.noreply.github.com>
2026-06-03 16:37:04 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-tetris-vishakha" # Replace with your actual S3 bucket name
+    bucket = "devsecops-tetris-vishakha-16" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
